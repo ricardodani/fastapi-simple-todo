74399a64e28e612e9753f1f02653c0a2141da9e6
Ricardo Dani <ricardodani@gmail.com>
2020-07-03 05:07:19 -0300

    Removed unused code

--- app/core/security.py
+++ app/core/security.py
@@ -1,5 +1,4 @@
 import hashlib
-from base64 import b64encode
 from fastapi import status, Depends, HTTPException
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
@@ -9,11 +8,6 @@ http_security = HTTPBasic()
 
 def get_password_hash(password: str) -> str:
     return hashlib.sha512(password.encode()).hexdigest()
-
-
-def get_auth_header(username: str, password: str) -> dict:
-    basic_token = b64encode(f"{username}:{password}".encode()).decode()
-    return dict(Authorization=f"Basic {basic_token}")
 
 
 async def authenticate(
